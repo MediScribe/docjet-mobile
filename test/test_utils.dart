144ce8c8596bb43f093c6a8e075be45381c32c08
Eckart Burgwedel <eburgwedel@gmail.com>
2025-04-08 19:31:41 +0200

    added test utils for easier import

---
+++ test/test_utils.dart
@@ -0,0 +1,43 @@
+/// Central utilities for testing
+///
+/// Import this file in your test files to get access to all common testing utilities.
+///
+/// ```dart
+/// import '../test_utils.dart';
+///
+/// void main() {
+///   setUpAll(setupTestLogging);
+///   tearDownAll(teardownTestLogging);
+///
+///   // Your tests...
+/// }
+/// ```
+
+// Import needed libraries first
+import 'package:docjet_mobile/core/utils/test_logger.dart';
+
+export 'package:docjet_mobile/core/utils/test_logger.dart';
+// Then export them
+export 'package:flutter_test/flutter_test.dart';
+
+// Optional: Add other common test utilities/mocking frameworks you use
+// export 'package:mocktail/mocktail.dart';
+// export 'package:fake_async/fake_async.dart';
+
+/// Convenience setup function for tests
+void setupTestLogging([LogLevel level = LogLevel.error]) {
+  TestLogger.setupTestFile(level);
+}
+
+/// Convenience teardown function for tests
+void teardownTestLogging() {
+  TestLogger.tearDownTestFile();
+}
+
+/// Enable logging for a specific set of tests
+void withLogging(LogLevel level, void Function() testCode) {
+  final prevLevel = TestLogger.logLevel;
+  TestLogger.setLogLevel(level);
+  testCode();
+  TestLogger.setLogLevel(prevLevel);
+}
